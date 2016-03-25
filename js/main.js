6ed4bbfa7dbf099184314bdf5bb4047f09fa9efa
Jaymie Thomas <jay@payzip.co.uk>
2016-03-25 17:28:47 +0000

    Made the first version

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(window).scroll(function() { // check if scroll event happened
         if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-            $(".navbar-fixed-top").css("background-color", "#c43234").css("height", "50").css("padding-top", 0); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+            $(".navbar-fixed-top").css("background-color", "#222C31").css("height", "50").css("padding-top", 0); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
         } else {
             $(".navbar-fixed-top").css("background-color", "transparent").css("height", "auto").css("padding-top", 25); // if not, change it back to transparent
         }
